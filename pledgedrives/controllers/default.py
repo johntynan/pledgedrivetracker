837e047820ea1afb079764e0f339114071472590
john <john@user-laptop>
2009-12-02 19:02:07 -0700

    refined index.html, added check for organization_id pledgedrive_id and segment_id in session

--- pledgedrives/controllers/default.py
+++ pledgedrives/controllers/default.py
@@ -17,50 +17,34 @@ def url(f, args=[]): return URL(r=request,f=f,args=args)
 error_page=URL(r=request,f='error')
 
 @auth.requires_login()
-def check_cookies():
-    organization_id = request.cookies['organization_id'].value if 'organization_id' in request.cookies else redirect(URL(r=request,f='set_cookies'))
-    pledgedrive_id = request.cookies['pledgedrive_id'].value if 'pledgedrive_id' in request.cookies else redirect(URL(r=request,f='set_cookies'))
+def check_session_organization_id():
+    if not request.function=='session_organization_id_form' and not session.organization_id:
+        redirect(URL(r=request, f='session_organization_id_form'))
 
+@auth.requires_login()
+def check_session_pledgedrive_id():
+    if not request.function=='session_pledgedrive_id_form' and not session.pledgedrive_id:
+        redirect(URL(r=request, f='session_pledgedrive_id_form'))
+
+@auth.requires_login()
+def check_session_segment_id():
+    if not request.function=='session_segment_id_form' and not session.segment_id:
+        redirect(URL(r=request, f='session_segment_id_form'))
+
+def check_session():
+    check_session_organization_id()
+    check_session_pledgedrive_id()
+    # segments are not required for challenges or programs
+    # check_session_segment_id()
+    # create check_session classes for programs and challenges when adding a segment
+    # check for persons when adding a challenge
+                
 def error():
     return dict(message='something is wrong')
 
 @auth.requires_login()
 def index():
-    # check_cookies()
-    """
-    example action using the internationalization operator T and flash
-    rendered by views/default/index.html or views/generic.html
-    """
-
-    response.flash = T('Welcome to the Pledge Drive Tracker')
-
-    # organizations=db(db.organization.id>0).select(orderby=db.organization.name)
-    # organization_id = request.cookies['organization_id'].value if 'organization_id' in request.cookies else 'unknown'
-
-    session.organization_id = 1
-    organization_id=session.organization_id
-    organizations=db(db.organization.id==organization_id).select()
-
-    # pledgedrive_id = request.cookies['pledgedrive_id'].value if 'pledgedrive_id' in request.cookies else 'unknown'
-    
-    session.pledgedrive_id = 1
-    
-    pledgedrive_id=session.pledgedrive_id
-
-    pledgedrives=db(db.pledgedrive.id==pledgedrive_id).select()
-
-    session.segment_id = 1
-    
-    segment_id=session.segment_id
-
-    segments=db(db.segment.id==segment_id).select()
- 
-    return dict(message=T('Pledge Drive Tracker'),organizations=organizations,organization_id=organization_id, pledgedrives=pledgedrives, pledgedrive_id=pledgedrive_id,segments=segments,segment_id=segment_id)
-
-    # return dict(message=T('Pledge Drive Tracker'))
-
-@auth.requires_login()  
-def set_cookies():
+    check_session()
     """
     example action using the internationalization operator T and flash
     rendered by views/default/index.html or views/generic.html
@@ -70,75 +54,85 @@ def set_cookies():
 
     organizations=db(db.organization.id>0).select(orderby=db.organization.name)
 
-    pledgedrives=db(db.pledgedrive.id>0).select(orderby=db.pledgedrive.title)
-
-    segments=db(db.segment.id>0).select(orderby=db.segment.title)
+    organization_id=session.organization_id
+    organization=db(db.organization.id==session.organization_id).select()
     
-    try:
-       organization_id = session.organization_id
-    except:
-       organization_id = 1
+    pledgedrive_id=session.pledgedrive_id
+    pledgedrive=db(db.pledgedrive.id==pledgedrive_id).select()
 
-    try:
-       pledgedrive_id = session.pledgedrive_id
-    except:
-       pledgedrive_id = 1               
+    segment_id=session.segment_id
+    segments=db(db.segment.id==segment_id).select()
+ 
+    return dict(message=T('Pledge Drive Tracker'),organization=organization,organizations=organizations,organization_id=organization_id, pledgedrive=pledgedrive, pledgedrive_id=pledgedrive_id,segments=segments,segment_id=segment_id)
 
-    try:
-       segment_id = session.segment_id
-    except:
-       segment_id = 1
+    # return dict(message=T('Pledge Drive Tracker'))
 
 
     return dict(message=T('Pledge Drive Tracker'),organizations=organizations,pledgedrives=pledgedrives, organization_id=organization_id, pledgedrive_id=pledgedrive_id,segments=segments,segment_id=segment_id)
-    
-def session_form():
-
-    if not session.organization_id:
-        organization_id = 1
- 
-    form = FORM('Set organization_id (current value is '+ str(session.organization_id) +') :',INPUT(_name='organization_id', _value=session.organization_id,requires=IS_NOT_EMPTY()),INPUT(_type='submit'))
-
-    # set the default year from the form
-    if FORM.accepts(form, request.post_vars, session,formname='form_one'):
-        session.organization_id = request.post_vars.organization_id
-        form.components[1].attributes['_value']=session.organization_id   # reset the default value of the INPUT field
-        # set the organization_id
-        response.flash = 'The organization_id is now '+session.organization_id
-    
-    # form = FORM('Set Organization: ',INPUT(_name='Organization', _value=session.organization_id,requires=IS_NOT_EMPTY()),INPUT(_type='submit'))
-    
-    record=db(db.organization.id==3).select()[0]
-    form=SQLFORM(db.organization, record)
-
-    if FORM.accepts(form,request.vars):
-        session.form_vars=form.vars 
-
-    return dict(form=form)
 
 
 @auth.requires_login()
+def session_organization_id_form():
+
+    organizations=db(db.organization.id>0).select(orderby=db.organization.name)
+
+    form = FORM(INPUT(_name='organization_id', requires=IS_NOT_EMPTY()), INPUT(_type='submit'))
+    # form = SQLFORM(db.organization.id>0).select(orderby=db.organization.name)
+    if form.accepts(request.vars, session):
+        session.organization_id = form.vars.organization_id
+        redirect(URL(r=request, f='index'))
+
+    return dict(form=form,organizations=organizations)
+
+@auth.requires_login()
+def session_pledgedrive_id_form():
+
+    pledgedrives=db(db.pledgedrive.id>0).select(orderby=db.pledgedrive.title)
+
+    form = FORM(INPUT(_name='pledgedrive_id', requires=IS_NOT_EMPTY()), INPUT(_type='submit'))
+    # form = SQLFORM(db.pledgedrive.id>0).select(orderby=db.pledgedrive.name)
+    if form.accepts(request.vars, session):
+        session.pledgedrive_id = form.vars.pledgedrive_id
+        redirect(URL(r=request, f='index'))
+
+    return dict(form=form,pledgedrives=pledgedrives)
+
+@auth.requires_login()
+def session_segment_id_form():
+
+    segments=db(db.segment.id>0).select(orderby=db.segment.title)
+
+    form = FORM(INPUT(_name='segment_id', requires=IS_NOT_EMPTY()), INPUT(_type='submit'))
+    # form = SQLFORM(db.segment.id>0).select(orderby=db.segment.name)
+    if form.accepts(request.vars, session):
+        session.segment_id = form.vars.segment_id
+        redirect(URL(r=request, f='index'))
+
+    return dict(form=form,segments=segments)
+
+    
+@auth.requires_login()
 def create_organization():
-    # check_cookies()
+    # check_session()
     form=crud.create(db.organization,next=url('list_organizations'))
     return dict(form=form)
 
 @auth.requires_login()
 def view_organization():
-    # check_cookies()
+    check_session()
     organization_id=request.args(0)
     organization=db.organization[organization_id] or redirect(error_page)
     return dict(organization=organization)
 
 @auth.requires_login()
 def list_organizations():
-    # check_cookies()
+    check_session()
     organizations=db(db.organization.id>0).select(orderby=db.organization.name)
     return dict(organizations=organizations)
 
 @auth.requires_login()
 def edit_organization():
-    # check_cookies()
+    check_session()
     organization_id=request.args(0)
     organization=db.organization[organization_id] or redirect(error_page)
     form=crud.update(db.organization,organization,next=url('list_organizations'))
@@ -146,7 +140,7 @@ def edit_organization():
 
 @auth.requires_login()
 def create_person():
-    # check_cookies()
+    check_session_organization_id()
     organization_id=request.args(0)
     organization=db.organization[organization_id] or redirect(error_page)
     db.person.organization.writable=False
@@ -156,20 +150,20 @@ def create_person():
 
 @auth.requires_login()
 def view_person():
-    # check_cookies()
+    check_session()
     person_id=request.args(0)
     person=db.person[person_id] or redirect(error_page)
     return dict(person=person)
 
 @auth.requires_login()
 def list_persons():
-    # check_cookies()
+    check_session()
     persons=db(db.person.id>0).select(orderby=db.person.name)
     return dict(persons=persons)
 
 @auth.requires_login()
 def edit_person():
-    # check_cookies()
+    check_session()
     person_id=request.args(0)
     person=db.person[person_id] or redirect(error_page)
     form=crud.update(db.person,person,next=url('view_person',person_id))
@@ -177,7 +171,7 @@ def edit_person():
 
 @auth.requires_login()    
 def list_persons_by_organization():
-    # check_cookies()
+    check_session()
     organization_id=request.args(0)
     organization=db.organization[organization_id] or redirect(error_page)
     persons=db(db.person.organization==organization.id).select(orderby=db.person.name)
@@ -185,7 +179,7 @@ def list_persons_by_organization():
 
 @auth.requires_login()
 def create_program():
-    # check_cookies()
+    check_session_organization_id()
     organization_id=request.args(0)
     organization=db.organization[organization_id] or redirect(error_page)
     db.program.organization.writable=False
@@ -195,20 +189,20 @@ def create_program():
 
 @auth.requires_login()
 def view_program():
-    # check_cookies()
+    check_session()
     program_id=request.args(0)
     program=db.program[program_id] or redirect(error_page)
     return dict(program=program)
 
 @auth.requires_login()
 def list_programs():
-    # check_cookies()
+    check_session()
     programs=db(db.program.id>0).select(orderby=db.program.title)
     return dict(programs=programs)
 
 @auth.requires_login()
 def edit_program():
-    # check_cookies()
+    check_session()
     program_id=request.args(0)
     program=db.program[program_id] or redirect(error_page)
     form=crud.update(db.program,program,next=url('view_program',program_id))
@@ -216,7 +210,7 @@ def edit_program():
 
 @auth.requires_login()
 def list_programs_by_organization():
-    # check_cookies()
+    check_session()
     organization_id=request.args(0)
     organization=db.organization[organization_id] or redirect(error_page)
     programs=db(db.program.organization==organization.id).select(orderby=db.program.title)
@@ -224,32 +218,26 @@ def list_programs_by_organization():
 
 @auth.requires_login()
 def create_pledgedrive():
-    # check_cookies()
-    # organization_id=request.args(0)
-    organization_id = request.cookies['organization_id'].value if 'organization_id' in request.cookies else redirect(URL(r=request,f='set_cookies'))
-    # organization=db.organization[organization_id] or redirect(error_page)
-    organization=db.organization[organization_id]
-    db.pledgedrive.organization.writable=True
-    db.pledgedrive.organization.readable=True
-    form=crud.create(db.pledgedrive)
-    return dict(form=form,organization=organization)
+    check_session_organization_id()
+    form=crud.create(db.pledgedrive,next=url('list_pledgedrives'))
+    return dict(form=form)
 
 @auth.requires_login()
 def view_pledgedrive():
-    # check_cookies()
+    check_session()
     pledgedrive_id=request.args(0)
     pledgedrive=db.pledgedrive[pledgedrive_id] or redirect(error_page)
     return dict(pledgedrive=pledgedrive)
 
 @auth.requires_login()
 def list_pledgedrives():
-    # check_cookies()
+    check_session()
     pledgedrives=db(db.pledgedrive.id>0).select(orderby=db.pledgedrive.title)
     return dict(pledgedrives=pledgedrives)
 
 @auth.requires_login()
 def edit_pledgedrive():
-    # check_cookies()
+    check_session()
     pledgedrive_id=request.args(0)
     pledgedrive=db.pledgedrive[pledgedrive_id] or redirect(error_page)
     form=crud.update(db.pledgedrive,pledgedrive,next=url('view_pledgedrive',pledgedrive_id))
@@ -257,7 +245,7 @@ def edit_pledgedrive():
 
 @auth.requires_login()
 def list_pledgedrives_by_organization():
-    # check_cookies()
+    check_session_organization_id()
     organization_id=request.args(0)
     organization=db.organization[organization_id] or redirect(error_page)
     pledgedrives=db(db.pledgedrive.organization==organization.id).select(orderby=db.pledgedrive.title)
@@ -267,12 +255,11 @@ def list_pledgedrives_by_organization():
 
 @auth.requires_login()
 def create_challenge():
-    # check_cookies()
+    check_session()
     organization=session.organization_id
     pledgedrive_id=request.args(0)
+    # pledgedrive_id=session.pledgedrive_id
     pledgedrive=db.pledgedrive[pledgedrive_id] or redirect(error_page)
-    # db.segment.pledgedrive.writable=False
-    # db.segment.pledgedrive.readable=False
     form=crud.create(db.challenge)
     return dict(form=form,pledgedrive=pledgedrive,organization=organization)
 
@@ -284,7 +271,7 @@ def view_challenge():
 
 @auth.requires_login()
 def list_challenges():
-    # check_cookies()
+    check_session()
     challenges=db(db.challenge.id>0).select(orderby=db.challenge.title)
     return dict(challenges=challenges)
 
@@ -297,7 +284,7 @@ def edit_challenge():
 
 @auth.requires_login()
 def list_challenges_by_pledgedrive():
-    # check_cookies()
+    check_session()
     pledgedrive_id=request.args(0)
     pledgedrive=db.pledgedrive[pledgedrive_id] or redirect(error_page)
     challenges=db(db.challenge.pledgedrive==pledgedrive.id).select(orderby=db.challenge.title)
@@ -305,18 +292,13 @@ def list_challenges_by_pledgedrive():
 
 @auth.requires_login()
 def create_segment():
-    # check_cookies()
-    organization=session.organization_id
-    pledgedrive_id=request.args(0)
-    pledgedrive=db.pledgedrive[pledgedrive_id] or redirect(error_page)
-    # db.segment.pledgedrive.writable=False
-    # db.segment.pledgedrive.readable=False
-    form=crud.create(db.segment)
-    return dict(form=form,pledgedrive=pledgedrive,organization=organization)
+    check_session_organization_id()
+    form=crud.create(db.segment,next=url('list_segments'))
+    return dict(form=form)
 
 @auth.requires_login()
 def view_segment():
-    # check_cookies()
+    check_session()
     segment_id=request.args(0)
     segment=db.segment[segment_id] or redirect(error_page)
     return dict(segment=segment)
@@ -328,7 +310,7 @@ def list_segments():
 
 @auth.requires_login()
 def list_segments_by_pledgedrive():
-    # check_cookies()
+    check_session()
     pledgedrive_id=request.args(0)
     pledgedrive=db.pledgedrive[pledgedrive_id] or redirect(error_page)
     segments=db(db.segments.pledgedrive==pledgedrive.id).select(orderby=db.segments.start_time)
@@ -338,7 +320,7 @@ def list_segments_by_pledgedrive():
 
 @auth.requires_login()
 def edit_segment():
-    # check_cookies()
+    check_session()
     segment_id=request.args(0)
     segment=db.segment[segment_id] or redirect(error_page)
     form=crud.update(db.segment,segment,next=url('view_segment',segment_id))
@@ -346,7 +328,7 @@ def edit_segment():
 
 @auth.requires_login()
 def list_segments_by_pledgedrive():
-    # check_cookies()
+    check_session()
     pledgedrive_id=request.args(0)
     pledgedrive=db.pledgedrive[pledgedrive_id] or redirect(error_page)
     segments=db(db.segment.pledgedrive==pledgedrive.id).select(orderby=db.segment.title)
@@ -354,7 +336,7 @@ def list_segments_by_pledgedrive():
 
 @auth.requires_login()
 def create_pledge():
-    # check_cookies()
+    check_session()
     segment_id=request.args(0)
     segment=db.segment[segment_id] or redirect(error_page)
     form=crud.create(db.pledge)
@@ -362,20 +344,20 @@ def create_pledge():
 
 @auth.requires_login()
 def view_pledge():
-    # check_cookies()
+    check_session()
     pledge_id=request.args(0)
     pledge=db.pledge[pledge_id] or redirect(error_page)
     return dict(pledge=pledge)
 
 @auth.requires_login()
 def list_pledges():
-    # check_cookies()
+    check_session()
     pledges=db(db.pledge.id>0).select(orderby=db.pledge.name)
     return dict(pledges=pledges)
 
 @auth.requires_login()
 def list_pledges_by_pledgedrive():
-    # check_cookies()
+    check_session()
     pledgedrive_id=request.args(0)
     pledgedrive=db.pledgedrive[pledgedrive_id] or redirect(error_page)
     pledges=db(db.pledge.pledgedrive==pledgedrive.id).select(orderby=db.pledge.id)
@@ -383,7 +365,7 @@ def list_pledges_by_pledgedrive():
 
 @auth.requires_login()
 def list_pledges_by_segment():
-    # check_cookies()
+    check_session()
     segment_id=request.args(0)
     pledgedrive=db.pledgedrive[segment_id] or redirect(error_page)
     pledges=db(db.pledge.segment==segment_id).select(orderby=db.pledge.id)
@@ -391,7 +373,7 @@ def list_pledges_by_segment():
         
 @auth.requires_login()
 def edit_pledge():
-    # check_cookies()
+    check_session()
     pledge_id=request.args(0)
     pledge=db.pledge[pledge_id] or redirect(error_page)
     form=crud.update(db.pledge,pledge,next=url('view_pledge',pledge_id))
@@ -399,53 +381,53 @@ def edit_pledge():
 
 @auth.requires_login()
 def list_pledges_by_pledgedrive():
-    # check_cookies()
+    check_session()
     pledgedrive_id=request.args(0)
     pledgedrive=db.pledgedrive[pledgedrive_id] or redirect(error_page)
     pledges=db(db.pledge.pledgedrive==pledgedrive.id).select(orderby=db.pledge.id)
     return dict(pledgedrive=pledgedrive, pledges=pledges)
 
 def frame_header():
-    # check_cookies()
+    check_session()
     # organization_id = request.cookies['organization_id'].value if 'organization_id' in request.cookies else 'unknown'
     organization_id = session.organization_id
     organizations=db(db.organization.id==organization_id).select()
     return dict(message=T('Pledge Drive Tracker'),organizations=organizations,organization_id=organization_id)
 
 def frame_wrapper_onair():
-    # check_cookies()
+    check_session()
     return dict()
 
 def frame_onair():
-    # check_cookies()
+    check_session()
     return dict()
 
 def frame_wrapper_pledge_entry():
-    # check_cookies()
+    check_session()
     return dict()
 
 def frame_pledge_entry():
-    # check_cookies()
+    check_session()
     return dict()
 
 def report_mini_segment_goal():
-    # check_cookies()
+    check_session()
     return dict()
 
 def report_mini_segment_challenge():
-    # check_cookies()
+    check_session()
     return dict()
 
 def report_mini_segment_select():
-    # check_cookies()
+    check_session()
     return dict()
 
 def report_mini_segment_overview():
-    # check_cookies()
+    check_session()
     return dict()
 
 def report_mini_pledgedrive_goal():
-    # check_cookies()
+    check_session()
     return dict()
 
 def create_message():
